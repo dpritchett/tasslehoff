6e463426bd42c440b3ab8338a05bb23669760fb2
Daniel J. Pritchett <dpritchett@gmail.com>
2010-10-07 11:46:28 -0500

    added command line argument

--- client.js
+++ client.js
@@ -9,16 +9,27 @@ var sys = require('sys'),
     WebSocket = require('websocket-client').WebSocket,
     ioutils = require('./support/socket.io/lib/socket.io/utils.js');
 
-var client = new WebSocket('ws://localhost:80/socket.io/websocket');
+//process command-line arguments i.e.
+//$ node client.js dpritchett.no.de
+var $SERVER = ( process.argv[2] || "localhost:80")
+if($SERVER.search(":") == -1){
+        $SERVER += ":80";
+}
+
+
+var client = new WebSocket('ws://' + $SERVER + '/socket.io/websocket');
 
 // Note that decode returns an array of messages, we're just handling the first for now
 client.onmessage = function(m) {
         m = ioutils.decode(m)[0];
         console.log('Got message: ' + sys.inspect(m));
-        if (m.substr(0, 3) == '~h~'){
-                client.send(
-                                ioutils.encode('~h~' + m.substr(3)));
-        }
+        // server will drop us if we don't respond to 'heartbeats'
+        // by resending the same text asap
+        var heartbeat = '~h~'
+                if (m.substr(0, 3) == heartbeat){
+                        client.send(
+                                        ioutils.encode(heartbeat + m.substr(3)));
+                }
 };
 
 // had to delay this because i was sending before the connection was up
